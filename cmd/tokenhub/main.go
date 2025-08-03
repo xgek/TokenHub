bb058cb736e536cbe61152dfdedfee1edf6aa0e2
XGEK <73622401+xgek@users.noreply.github.com>
2025-08-03 19:18:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tokenhub/main.go
@@ -0,0 +1,23 @@
+// cmd/tokenhub/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"tokenhub/internal/tokenhub"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := tokenhub.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
